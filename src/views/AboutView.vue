aa5c0f2d6a15537bb4457b501f66149d131ec694
Luis Ortiz <luiann.ortiz@gmail.com>
2022-04-29 13:45:36 -0500

    Add card to pokemon, installa axios

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <PokemonCard />
   </div>
 </template>
+<script>
+import PokemonCard from '@/components/Home/PokemonCard.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    PokemonCard
+  }
+}
+</script>
+
