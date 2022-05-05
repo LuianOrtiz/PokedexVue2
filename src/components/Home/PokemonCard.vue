83d79d4cec5e565673fd9ef17f6b89ac155354d0
Luian Ortiz <luiann.ortiz@gmail.com>
2022-05-05 16:32:13 -0500

    add peticiones get

--- src/components/Home/PokemonCard.vue
+++ src/components/Home/PokemonCard.vue
@@ -2,35 +2,52 @@
     <b-card  class="overflow-hidden" style="max-width: 540px;">
         <b-row>
             <b-col md="6">
-            <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+            <b-card-img src="https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-2.png" alt="Image" class="rounded-0"></b-card-img>
             </b-col>
             <b-col md="6">
-            <b-card-body title="Nombre">
-                <b-card-text>
-                <!--Description -->
-                This is a wider card with supporting text as a natural lead-in to additional content.
-                <div class="skills">
-                    <p> Habilidades:  </p>
-                    <p> Tipo: </p>
-                </div>
-                </b-card-text>
-            </b-card-body>
+                <b-card-body>
+                    <b-card-text>
+                        <!--Description -->
+                        <p> {{pokemon.nombre}} </p> 
+                        {{pokemon.descripcion}}
+                        <p> Habilidad: {{pokemon.habilidad}} </p>
+                        <p> Tipo: {{pokemon.tipo}} </p>
+                    </b-card-text>
+                </b-card-body>
             </b-col>
         </b-row>
-        <div class="buttons">
-            <b-button variant="outline-primary" size="sm">Escoger Pokemon </b-button>
-            <b-button variant="outline-warning" size="sm">Ver detalles </b-button>
-        </div>
+            <b-button variant="outline-primary" size="sm" class="buttons" @click="hola()">Escoger Pokemon </b-button>
+            <b-button variant="outline-warning" size="sm" class="buttons">Ver detalles </b-button>
     </b-card>
 </template>
 
 <script>
 export default {
-    name: 'PokemonCard'
+    name: 'PokemonCard',
+    data(){
+        return {
+
+        }
+    },
+
+    props: {
+        pokemon: Object
+        
+    },
+
+    methods: {
+        hola(){
+            console.log(this.pokemon);
+        }
+    }
 }
 </script>
+
+
 <style scoped>
-.skills {
-    margin-left: -100px;
+
+.buttons {
+    padding: 0 10px;
+    margin-right: 60px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,19 @@
   <div class="home">
     <b-container class="bv-example-row" fluid>
       <b-row>
-        
         <b-col>
           <br>
-          <PokemonCard />
+          <PokemonCard :pokemon=this.pokemon />
           <br>
         </b-col>
         <b-col>
           <br>
-          <PokemonCard />
+          <PokemonCard :pokemon=this.pokemon />
           <br>
         </b-col>
         <b-col>
           <br>
-          <PokemonCard />
+          <PokemonCard :pokemon=this.pokemon />
           <br>
         </b-col>
         <b-col>
@@ -24,6 +23,7 @@
         </b-col>
       </b-row>
     </b-container>
+	<button @click="getPokemon">boton</button>
   </div>
 </template>
 
@@ -31,16 +31,58 @@
 // @ is an alias to /src
 import PokemonCard from '@/components/Home/PokemonCard.vue'
 import PokemonElection from '@/components/Home/PokemonElection.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      pokemon: {
+		imagen: "",
+        nombre: "",
+        descripcion: "",
+        habilidad: ["",""],
+        tipo: ["", ""]
+      }
+    }
+  },  
   components: {
     PokemonCard,
-    PokemonElection
+	PokemonElection
   },
 
   methods: {
+	async getPokemones(){
+		const url_api_pokemon = "https://pokeapi.co/api/v2/pokemon/";
+			try {
+				await axios.get(url_api_pokemon).
+				then( (response) => {
+					console.log(response.data.results[0]);
+					}
+				)	  
+			} catch (error) {
+				this.message = 'error'
+			}
+	},
 
+	async getPokemon() {
+		const url_pokemon = "https://pokeapi.co/api/v2/pokemon/";
+		try {
+			var pokemonNombre = this.pokemon.nombre
+			await axios.get(url_pokemon+pokemonNombre).
+			then( (response) => {
+				this.pokemon.nombre = response.data.name;
+				this.pokemon.habilidad[0] = response.data.abilities[0].ability.name
+				this.pokemon.habilidad[1] = response.data.abilities[1].ability.name
+				this.pokemon.tipo[0] = response.data.types[0].type.name
+				this.pokemon.tipo[1] = response.data.types[1].type.name
+			}
+
+			)
+		} catch (error) {
+			this.message = 'error'
+		}
+	}
 
   }
 
